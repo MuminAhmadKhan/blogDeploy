2d010ac6705b0418f00d355218a12b4a162d848c
MaK <muminkhan2000@gmail.com>
2022-06-18 18:19:16 +0530

    try

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const  middleware = require('./utils/middelware')
 const loginRouter = require('./controllers/login')
 
 mongoose.connect(MONGODB_URI)
-app.use(express.static('build')) 
+
 app.use(middleware.tokenExtractor)
 
 app.use(cors())
@@ -20,7 +20,6 @@ app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users',userRouter)
 app.use(middleware.requestLogger)
-
 if (process.env.NODE_ENV === 'test') {
     const testingRouter = require('./controllers/testing')
     app.use('/api/testing', testingRouter)
