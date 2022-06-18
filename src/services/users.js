4364796c21737531bfc56d0b52e3d03404b21b7d
MaK <muminkhan2000@gmail.com>
2022-06-18 18:29:11 +0530

    try

--- src/services/users.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/users'
-
-const getAll = async() => {
-  const request = await axios.get(baseUrl)
-  return request.data
-}
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {getAll}
