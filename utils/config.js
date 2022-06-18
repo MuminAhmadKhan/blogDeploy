5b1fc0f4afeac2b8bb2e62e5af8107779ba46f85
MaK <muminkhan2000@gmail.com>
2022-06-18 13:47:16 +0530

    try

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
  const MONGODB_URI = process.env.NODE_ENV === 'test' 
  ? process.env.TEST_MONGO_URI
  : process.env.MONGO_URI
+ console.log(MONGODB_URI)
  module.exports={
      PORT,
      MONGODB_URI
